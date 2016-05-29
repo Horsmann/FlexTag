e4eecba0abd56de9be567dfef61ab44e64c0edeb
Tobias Horsmann <tobias.horsmann@gmail.com>
2016-05-29 15:55:16 +0200

    Issue #1: bugfix in wrapper

--- flextag-core/src/main/java/de/unidue/ltl/flextag/core/uima/TcPosTaggingWrapper.java
+++ flextag-core/src/main/java/de/unidue/ltl/flextag/core/uima/TcPosTaggingWrapper.java
@@ -1,23 +1,5 @@
-/*******************************************************************************
- * Copyright 2016
- * Language Technology Lab
- * University of Duisburg-Essen
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ******************************************************************************/
 package de.unidue.ltl.flextag.core.uima;
 
-import java.util.Collection;
 import java.util.List;
 
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
@@ -51,7 +33,7 @@ public class TcPosTaggingWrapper
                     sent.getBegin(), sent.getEnd());
             sequence.addToIndexes();
 
-            Collection<Token> tokens = JCasUtil.select(aJCas, Token.class);
+            List<Token> tokens = JCasUtil.selectCovered(aJCas, Token.class, sent);
 
             for (Token token : tokens) {
                 TextClassificationUnit unit = new TextClassificationUnit(aJCas, token.getBegin(),
