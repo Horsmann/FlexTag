5843b903ae459344c44ca0c67fbd5aa7228a6b50
Tobias Horsmann <tobias.horsmann@gmail.com>
2016-10-30 20:56:43 +0100

    #7 libsvm cv example

---
+++ flextag-examples/src/main/java/de/unidue/ltl/flextag/examples/cv/ExampleLibsvmCrossValidation.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright 2016
+ * Language Technology Lab
+ * University of Duisburg-Essen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+package de.unidue.ltl.flextag.examples.cv;
+
+import static java.util.Arrays.asList;
+
+import java.util.List;
+
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.factory.CollectionReaderFactory;
+import org.dkpro.tc.api.features.TcFeatureFactory;
+import org.dkpro.tc.features.length.NrOfChars;
+import org.dkpro.tc.features.ngram.LuceneCharacterNGram;
+import org.dkpro.tc.features.ngram.LuceneNGram;
+import org.dkpro.tc.ml.libsvm.LibsvmAdapter;
+
+import de.unidue.ltl.flextag.core.Classifier;
+import de.unidue.ltl.flextag.core.FlexTagCrossValidation;
+import de.unidue.ltl.flextag.core.reports.adapter.cv.CvLibLinearSvmAvgKnownUnknownAccuracyReport;
+import de.unidue.ltl.flextag.examples.util.DemoConstants;
+import de.unidue.ltl.flextag.examples.util.LineTokenTagReader;
+
+public class ExampleLibsvmCrossValidation
+{
+    public static void main(String[] args)
+        throws Exception
+    {
+        new ExampleLibsvmCrossValidation().run();
+    }
+
+    public void run()
+        throws Exception
+    {
+        String language = "en";
+        String trainCorpora = DemoConstants.TRAIN_FOLDER_CROSS_VALIDATION;
+        String trainFileSuffix = "*.txt";
+
+        CollectionReaderDescription crd = CollectionReaderFactory.createReaderDescription(
+                LineTokenTagReader.class, LineTokenTagReader.PARAM_LANGUAGE, language,
+                LineTokenTagReader.PARAM_SOURCE_LOCATION, trainCorpora,
+                LineTokenTagReader.PARAM_PATTERNS, trainFileSuffix,
+                LineTokenTagReader.PARAM_SEQUENCES_PER_CAS, 1);
+
+        FlexTagCrossValidation flex = new FlexTagCrossValidation(crd, 2);
+
+        if (System.getProperty("DKPRO_HOME") == null) {
+            flex.setDKProHomeFolder("target/home");
+        }
+        flex.setExperimentName("LibsvmCrossValidationDemo");
+
+        // we additionally add a brown cluster and specify that we want to keep using the default
+        // feature set, setting the last parameter to "false" will remove the default feature set
+        // and only use the here specified features will be used.
+        flex.setFeatures(TcFeatureFactory.create(NrOfChars.class),
+                TcFeatureFactory.create(LuceneCharacterNGram.class),
+                TcFeatureFactory.create(LuceneNGram.class));
+
+        List<Object> configuration = asList(
+                new Object[] { "-s", LibsvmAdapter.PARAM_SVM_TYPE_C_SVC_MULTI_CLASS });
+        flex.setClassifier(Classifier.LIBSVM, configuration);
+        flex.addReport(CvLibLinearSvmAvgKnownUnknownAccuracyReport.class);
+        flex.execute();
+    }
+
+}
